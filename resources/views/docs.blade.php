339a9887f8823e6c0256be6d61458a53c2c0917c
michaelynm <michaelmarcelino55@gmail.com>
2025-08-16 11:07:11 +0700

    feat: add api docs

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = () => {
+      SwaggerUIBundle({ url: "/openapi.yaml", dom_id: "#swagger-ui" });
+    };
+  </script>
+</body>
+</html>
